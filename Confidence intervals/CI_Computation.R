1366a08148b0577bb12eb99cd5d9f1161c2111bb
Marie Delacre <marie.delacre@ulb.ac.be>
2020-03-03 20:13:13 +0100

    Replace alpha with conf.level

--- Confidence intervals/CI_Computation.R
+++ Confidence intervals/CI_Computation.R
@@ -9,12 +9,12 @@ for (package in "rootSolve") {
 #########################################
 ##  Simulation - Confidence intervals  ##
 #########################################
-
+ 
 #--------------------------------------------------------------
 #  Obtain Student confidence limits following Cohen's procedure
 #--------------------------------------------------------------
 
-Cohen.CI <- function(Group.1, Group.2,alpha)
+Cohen.CI <- function(Group.1, Group.2,conf.level)
 { 
   n1 <- length(Group.1)
   n2 <- length(Group.2)
@@ -24,9 +24,9 @@ Cohen.CI <- function(Group.1, Group.2,alpha)
   
   #  find limits of lambda at pt = alpha/2 or 1-alpha/2
   
-  # Limit of lambda such as pt(q=t_obs, df=n1+n2-2, ncp = lambda) = alpha/2
+  # Limit of lambda such as pt(q=t_obs, df=n1+n2-2, ncp = lambda) = (1-conf.level)/2 = alpha/2
   f=function(lambda,rep) pt(q=t_obs, df=n1+n2-2, ncp = lambda)-rep
-  out=uniroot(f,c(-100,100),rep=alpha/2)
+  out=uniroot(f,c(-100,100),rep=(1-conf.level)/2)
   lambda.1 <- out$root
   delta.1 <- lambda.1*sqrt(1/n1+1/n2)   # Pr[t_obs(alpha/2,df=df,ncp=lambda) <= lambda <= t_obs(1-alpha/2,df=df,ncp=lambda)=.025
                                         # because lambda = delta * sqrt[n1n2/(n1+n2)] :
@@ -34,9 +34,9 @@ Cohen.CI <- function(Group.1, Group.2,alpha)
                                         # Pr[t_obs(alpha/2,df=df,ncp=lambda)*sqrt[(n1+n2)/n1n2] <= delta <= t_obs(1-alpha/2,df=df,ncp=lambda)*sqrt[(n1+n2)/n1n2]=.025
                                         # and sqrt[(n1+n2)/n1n2]=sqrt(1/n1+1/n2)
 
-  # Limit of lambda such as 1-pt(q=t_obs, df=n1+n2-2, ncp = lambda) = alpha/2
+  # Limit of lambda such as 1-pt(q=t_obs, df=n1+n2-2, ncp = lambda) = (1-conf.level)/2 = alpha/2
   f=function(lambda,rep) 1-pt(q=t_obs, df=n1+n2-2, ncp = lambda)-rep
-  out=uniroot(f,c(-100,100),rep=alpha/2)
+  out=uniroot(f,c(-100,100),rep=(1-conf.level)/2)
   lambda.2 <- out$root
   delta.2 <- lambda.2*sqrt(1/n1+1/n2)   # See explanation for delta.1
 
@@ -46,19 +46,6 @@ Cohen.CI <- function(Group.1, Group.2,alpha)
   result <- c(delta.low, delta.upp) 
   return(result)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
