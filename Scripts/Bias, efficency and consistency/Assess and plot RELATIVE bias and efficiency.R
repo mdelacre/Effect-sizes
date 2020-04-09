44c4aa8e5445b8478a37581d66cf17c0be96a05f
Marie Delacre <marie.delacre@ulb.ac.be>
2020-04-09 13:06:22 +0200

    minor modifications in script

--- Scripts/Bias, efficency and consistency/Assess and plot RELATIVE bias and efficiency.R
+++ Scripts/Bias, efficency and consistency/Assess and plot RELATIVE bias and efficiency.R
@@ -10,8 +10,8 @@ Folder=paste0(Mainfolder,subfolder)
 for (i in seq_len(length(Folder))){
 
   # set up empty container for all estimated parameters
-  good_mes <-matrix(0,length(list.files(Folder[i])),5+3*6)
-  goodness_indic <- c("bias_","eff_","MSE_")
+  good_mes <-matrix(0,length(list.files(Folder[i])),5+2*6)
+  goodness_indic <- c("bias_","relbias_")
   estimator <- c("Cohen","Hedge","Glass1","Glass2","Shieh", "Shieh_corr")
   columns_names=expand.grid(estimator,goodness_indic)
   colnames(good_mes) <- c("n1","n2","n1/n2","m1-m2","sd1/sd2",paste0(columns_names[,2],columns_names[,1]))
@@ -39,30 +39,22 @@ for (i in seq_len(length(Folder))){
     sigma_unbal <- sqrt((1-n1/(n1+n2))*sd1^2 + (1-n2/(n1+n2))*sd2^2)    
     sigma_bal <-   sqrt((sd1^2+sd2^2)/2)
     shieh_delta_corr <- shieh_delta*(((nratio+1)*sigma_unbal)/(2*sigma_bal*sqrt(nratio)))  
+
+    # Compute relative bias
+    bias_cohen <- (mean(file[,9]) - cohen_delta)
+    bias_hedge <- (mean(file[,10]) - hedge_delta)
+    bias_glass1 <- (mean(file[,11]) - glass_delta1) 
+    bias_glass2 <- (mean(file[,12]) - glass_delta2)
+    bias_shieh <- (mean(file[,13]) - shieh_delta)
+    bias_shieh_corr <- (mean(file[,14]) - shieh_delta_corr)
     
-    # Compute bias
-    bias_cohen <- mean(file[,9]) - cohen_delta
-    bias_hedge <- mean(file[,10]) - hedge_delta
-    bias_glass1 <- mean(file[,11]) - glass_delta1 
-    bias_glass2 <- mean(file[,12]) - glass_delta2
-    bias_shieh <- mean(file[,13]) - shieh_delta
-    bias_shieh_corr <- mean(file[,14]) - shieh_delta_corr
-    
-    # Compute Efficiency
-    eff_cohen <- var(file[,9])
-    eff_hedge <- var(file[,10])
-    eff_glass1 <- var(file[,11])
-    eff_glass2 <- var(file[,12])
-    eff_shieh <- var(file[,13])
-    eff_shieh_corr <- var(file[,14])
-    
-    # MSE
-    MSE_cohen <- eff_cohen + bias_cohen^2
-    MSE_hedge <- eff_hedge + bias_hedge^2
-    MSE_glass1 <- eff_glass1 + bias_glass1^2
-    MSE_glass2 <- eff_glass2 + bias_glass2^2
-    MSE_shieh <- eff_shieh + bias_shieh^2
-    MSE_shieh_corr <- eff_shieh_corr + bias_shieh_corr^2
+    # Compute relative bias
+    relbias_cohen <- (mean(file[,9]) - cohen_delta)/cohen_delta
+    relbias_hedge <- (mean(file[,10]) - hedge_delta)/hedge_delta
+    relbias_glass1 <- (mean(file[,11]) - glass_delta1)/glass_delta1 
+    relbias_glass2 <- (mean(file[,12]) - glass_delta2)/glass_delta2
+    relbias_shieh <- (mean(file[,13]) - shieh_delta)/shieh_delta
+    relbias_shieh_corr <- (mean(file[,14]) - shieh_delta_corr)/shieh_delta_corr
     
     good_mes[j,1] <- n1
     good_mes[j,2] <- n2
@@ -77,19 +69,12 @@ for (i in seq_len(length(Folder))){
     good_mes[j,10] <- bias_shieh
     good_mes[j,11] <- bias_shieh_corr
     
-    good_mes[j,12] <- eff_cohen
-    good_mes[j,13] <- eff_hedge
-    good_mes[j,14] <- eff_glass1
-    good_mes[j,15] <- eff_glass2
-    good_mes[j,16] <- eff_shieh
-    good_mes[j,17] <- eff_shieh_corr
-    
-    good_mes[j,18] <- MSE_cohen
-    good_mes[j,19] <- MSE_hedge
-    good_mes[j,20] <- MSE_glass1
-    good_mes[j,21] <- MSE_glass2
-    good_mes[j,22] <- MSE_shieh
-    good_mes[j,23] <- MSE_shieh_corr
+    good_mes[j,12] <- relbias_cohen
+    good_mes[j,13] <- relbias_hedge
+    good_mes[j,14] <- relbias_glass1
+    good_mes[j,15] <- relbias_glass2
+    good_mes[j,16] <- relbias_shieh
+    good_mes[j,17] <- relbias_shieh_corr
     
   }
   
@@ -97,7 +82,7 @@ for (i in seq_len(length(Folder))){
   
   shapeparam<-str_extract_all(Folder[i], "[[:digit:]]+\\.*[[:digit:]]*")
   sub<-paste0("G1=",shapeparam[[1]][2],",G2=",shapeparam[[1]][4]) # Sign is not extracted but it's not a big deal
-  write.table(good_mes,paste0(sub,",good_mes.txt"),sep=";",dec=",")
+  write.table(good_mes,paste0(sub,",relative bias.txt"),sep=";",dec=",")
   
 }
 
@@ -140,30 +125,31 @@ legend("center",
 dev.off()
 
 Path <-  "C:/Users/Marie/Documents/Github_projects/Effect-sizes/Scripts outputs/Quality of ES measures/"
-for (j in seq_len(length(list.files(Path,pattern = ".*good_mes.txt")))){
+for (j in seq_len(length(list.files(Path,pattern = ".*relative bias.txt")))){
   
 
-  File=read.table(paste0(Path,list.files(Path,pattern = ".*good_mes.txt")[j]),header=T,sep=";",dec=",")  
+  File=read.table(paste0(Path,list.files(Path,pattern = ".*relative bias.txt")[j]),header=T,sep=";",dec=",")  
   
   # Subdivise file into five categories
   # "Hom_bal", "Hom_unbal", "Het_bal","Het_rpos","Het_rneg"
   
   ### Conditions id 
   
+  # We also need to exclude simulations where mu1-mu2=0 (on cannot compute relative bias when mu1-mu2=0...)
   # Homoscedasticity, balanced designs ("Hom_bal")
-  id_Hom_bal=as.numeric(rownames(File[(File$n1==File$n2)&(File$sd1.sd2 == 1),]))
+  id_Hom_bal=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1==File$n2)&(File$sd1.sd2 == 1),]))
   # Homoscedasticity, unbalanced designs ("Hom_unbal")  
-  id_Hom_nN=as.numeric(rownames(File[(File$n1<File$n2)&(File$sd1.sd2 == 1),]))
-  id_Hom_Nn=as.numeric(rownames(File[(File$n1>File$n2)&(File$sd1.sd2 == 1),]))
+  id_Hom_nN=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1<File$n2)&(File$sd1.sd2 == 1),]))
+  id_Hom_Nn=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1>File$n2)&(File$sd1.sd2 == 1),]))
   # Heteroscedasticity, balanced designs ("Het_bal")
-  id_sdSD_bal=as.numeric(rownames(File[(File$n1==File$n2)&(File$sd1.sd2 < 1),]))
-  id_SDsd_bal=as.numeric(rownames(File[(File$n1==File$n2)&(File$sd1.sd2 > 1),]))
+  id_sdSD_bal=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1==File$n2)&(File$sd1.sd2 < 1),]))
+  id_SDsd_bal=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1==File$n2)&(File$sd1.sd2 > 1),]))
   # Heteroscedasticity, positive correlation between n and sd ("Het_rpos")
-  id_sdSD_nN=as.numeric(rownames(File[(File$n1<File$n2)&(File$sd1.sd2 < 1),]))
-  id_SDsd_Nn=as.numeric(rownames(File[(File$n1>File$n2)&(File$sd1.sd2 > 1),]))
+  id_sdSD_nN=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1<File$n2)&(File$sd1.sd2 < 1),]))
+  id_SDsd_Nn=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1>File$n2)&(File$sd1.sd2 > 1),]))
   # Heteroscedasticity, negative correlation between n and sd ("Het_rneg")
-  id_SDsd_nN=as.numeric(rownames(File[(File$n1<File$n2)&(File$sd1.sd2 > 1),]))
-  id_sdSD_Nn=as.numeric(rownames(File[(File$n1>File$n2)&(File$sd1.sd2 < 1),]))
+  id_SDsd_nN=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1<File$n2)&(File$sd1.sd2 > 1),]))
+  id_sdSD_Nn=as.numeric(rownames(File[(File$m1.m2!=0)&(File$n1>File$n2)&(File$sd1.sd2 < 1),]))
   
   # Study each condition separately
   Conditions_id <- list(id_Hom_bal=id_Hom_bal,
@@ -197,51 +183,35 @@ for (j in seq_len(length(list.files(Path,pattern = ".*good_mes.txt")))){
     res.bias <- t(res[,3:8])
     colnames(res.bias) <- paste0(res[,1],":",res[,2])
     
-    # Matrix containing variances information
+    # Matrix containing relative bias information
     res2 <- matrix(0,9,8)  
-    names<-expand.grid("var_",c("Cohen","Hedge","Glass1","Glass2","Shieh","Shieh_corr"))
+    names<-expand.grid("relbias_",c("Cohen","Hedge","Glass1","Glass2","Shieh","Shieh_corr"))
     colnames(res2) <- c("n1","n2",paste0(names[,1],names[,2]))
     res2[,1:2] <- cbind(combi[,1],combi[,2]) 
-    res2[,3] <- tapply(Sel$eff_Cohen,list(Sel$n1,Sel$n2),mean)[1:9]
-    res2[,4] <- tapply(Sel$eff_Hedge,list(Sel$n1,Sel$n2),mean)[1:9]
-    res2[,5] <- tapply(Sel$eff_Glass1,list(Sel$n1,Sel$n2),mean)[1:9]
-    res2[,6] <- tapply(Sel$eff_Glass2,list(Sel$n1,Sel$n2),mean)[1:9]
-    res2[,7] <- tapply(Sel$eff_Shieh,list(Sel$n1,Sel$n2),mean)[1:9]
-    res2[,8] <- tapply(Sel$eff_Shieh_corr,list(Sel$n1,Sel$n2),mean)[1:9]
+    res2[,3] <- tapply(Sel$relbias_Cohen,list(Sel$n1,Sel$n2),mean)[1:9]
+    res2[,4] <- tapply(Sel$relbias_Hedge,list(Sel$n1,Sel$n2),mean)[1:9]
+    res2[,5] <- tapply(Sel$relbias_Glass1,list(Sel$n1,Sel$n2),mean)[1:9]
+    res2[,6] <- tapply(Sel$relbias_Glass2,list(Sel$n1,Sel$n2),mean)[1:9]
+    res2[,7] <- tapply(Sel$relbias_Shieh,list(Sel$n1,Sel$n2),mean)[1:9]
+    res2[,8] <- tapply(Sel$relbias_Shieh_corr,list(Sel$n1,Sel$n2),mean)[1:9]
     # Select only rows with no "NA"  
     res2 <- subset(res2,res2[,3] != "NA") 
     # Select only bias columns
-    res.eff <- t(res2[,3:8])
-    colnames(res.eff) <- paste0(res2[,1],":",res2[,2])
+    res.relbias <- t(res2[,3:8])
+    colnames(res.relbias) <- paste0(res2[,1],":",res2[,2])
     
-    # Matrix containing MSE information
-    res3 <- matrix(0,9,8)  
-    names<-expand.grid("MSE_",c("Cohen","Hedge","Glass1","Glass2","Shieh","Shieh_corr"))
-    colnames(res3) <- c("n1","n2",paste0(names[,1],names[,2]))
-    res3[,1:2] <- cbind(combi[,1],combi[,2])
-    res3[,3] <- tapply(Sel$MSE_Cohen,list(Sel$n1,Sel$n2),mean)[1:9]
-    res3[,4] <- tapply(Sel$MSE_Hedge,list(Sel$n1,Sel$n2),mean)[1:9]
-    res3[,5] <- tapply(Sel$MSE_Glass1,list(Sel$n1,Sel$n2),mean)[1:9]
-    res3[,6] <- tapply(Sel$MSE_Glass2,list(Sel$n1,Sel$n2),mean)[1:9]
-    res3[,7] <- tapply(Sel$MSE_Shieh,list(Sel$n1,Sel$n2),mean)[1:9]
-    res3[,8] <- tapply(Sel$MSE_Shieh_corr,list(Sel$n1,Sel$n2),mean)[1:9]
-    # Select only rows with no "NA"  
-    res3 <- subset(res3,res3[,3] != "NA") 
-    # Select only bias columns
-    res.MSE <- t(res3[,3:8])
-    colnames(res.MSE) <- paste0(res3[,1],":",res3[,2])
     
-    param <- str_extract_all(list.files(Path,pattern=".*good_mes.txt")[j], "[[:digit:]]+\\.*[[:digit:]]*")
+    param <- str_extract_all(list.files(Path,pattern=".*relative bias.txt")[j], "[[:digit:]]+\\.*[[:digit:]]*")
     G1 <- as.numeric(param[[1]][2])
     G2 <- as.numeric(param[[1]][4])
     
-     setwd("C:/Users/Marie/Documents/Github_projects/Effect-sizes/Scripts outputs/Quality of ES measures/Graphs")
+    # setwd("C:/Users/Marie/Documents/Github_projects/Effect-sizes/Scripts outputs/Quality of ES measures/Graphs")
     #dir.create(names(Conditions_id)[i])
     
     setwd(paste0("C:/Users/Marie/Documents/Github_projects/Effect-sizes/Scripts outputs/Quality of ES measures/Graphs/",names(Conditions_id)[i]))
-    png(file=paste0("bias_eff,G1=",G1, " & G2=",G2,";",names(Conditions_id)[i], ".png"),width=1000,height=1700, units = "px", res = 300)  
+    png(file=paste0("relativebias,G1=",G1, " & G2=",G2,";",names(Conditions_id)[i], ".png"),width=1000,height=1700, units = "px", res = 300)  
     
-    par(xpd = F,mar = c(4,5,1.5,0),mfrow = c(3,1))   
+    par(xpd = F,mar = c(4,5,1.5,0),mfrow = c(2,1))   
     
 
     # plot for the bias
@@ -259,43 +229,23 @@ for (j in seq_len(length(list.files(Path,pattern = ".*good_mes.txt")))){
             ylab=ylabelbias
             )
 
-
-    
     # plot for the variance
     
-    if (j==1){ylabeleff=expression(paste("Var(" , hat(delta) , ")"))
+    if (j==1){ylabeleff=expression(paste("[E(" , hat(delta) , ") -",delta,"]/",delta ))
     } else {ylabeleff=""}
     
-    barplot(res.eff, 
+    barplot(res.relbias, 
             col = c("black","grey25","grey50","grey70","grey90","white"),
             beside = TRUE,
             ylab = ylabeleff,
             xaxt="n",
             cex.lab=1.5,
             args.legend = list(
-              x = length(res.eff)*1.2,
-              y = max(res.eff)+.5,
+              x = length(res.relbias)*1.2,
+              y = max(res.relbias)+.5,
               bty="n"
             ))
-    
-    # plot for the MSE
-    
-    if (j==1){ylabelMSE=expression(paste( "MSE(",hat(delta), ")"))
-    } else {ylabelMSE=""}
-    
-    barplot(res.MSE, 
-            col = c("black","grey25","grey50","grey70","grey90","white"),
-            beside = TRUE,
-            ylab = ylabelMSE,
-            xlab = "n1 : n2",
-            cex.lab=1.5,
-            args.legend = list(
-              x = length(res.MSE)*1.2,
-              y = max(res.MSE)+.5,
-              bty="n"
-            ))
-    
-    
+
     dev.off()
     
   }
